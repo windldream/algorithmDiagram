928fc89b5460cde057acf0b49b4acdba2af1bb0a
windldream <1508353859@qq.com>
2019-05-24 11:17:42 +0800

    希尔排序

--- shellSort.js
+++ shellSort.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {number []} arr 要排序的数组
+ * @param {number []} gaps 增量序列
+ */
 function shellSort(arr, gaps) {
     for (let g = 0; g < gaps.length; g++) {
         for (let i = 0; i < arr.length; i++) {
@@ -14,21 +19,19 @@ function shellSort(arr, gaps) {
         }
     }
 }
+// shellSort(arr, [5, 3, 1]);
 
-/**
- * 
- * @param {number []} arr 要排序的数组
- * @param {number []} gaps 增量序列
- */
-function shellSort1(arr, gaps) {
-    for (let g = 0; g < gaps.length; g++) {
+
+function shellSort1(arr) {
+    let len = arr.length;
+    for (let gap = len >> 1; gap > 0; gap >>= 1) {
         for (let i = 0; i < arr.length; i++) {
             let temp = arr[i];
             let j;
-            for (j = i; j >= gaps[g] &&
-                    arr[j - gaps[g]] > temp; j -= gaps[g]) {
+            for (j = i; j >= gap &&
+                    arr[j - gap] > temp; j -= gap) {
                 // 交换法
-                swap(arr, j, j - gaps[g]);
+                swap(arr, j, j - gap);
             }
         }
     }
@@ -39,6 +42,6 @@ function shellSort1(arr, gaps) {
 }
 
 let arr = [3, 5, 1, 6, 4, 7, 2];
-shellSort1(arr, [5, 3, 1]);
 
+shellSort1(arr);
 console.log(arr);
