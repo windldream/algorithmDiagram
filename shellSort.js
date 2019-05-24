310fa277a3af49cde3fab83a31e2642332ac948f
windldream <1508353859@qq.com>
2019-05-24 10:55:39 +0800

    希尔排序

--- shellSort.js
+++ shellSort.js
@@ -5,6 +5,7 @@ function shellSort(arr, gaps) {
             let j;
             for (j = i; j >= gaps[g] &&
                     arr[j - gaps[g]] > temp; j -= gaps[g]) {
+                // 移动法
                 arr[j] = arr[j - gaps[g]];
                 console.log('arr', arr);
             }
@@ -13,7 +14,31 @@ function shellSort(arr, gaps) {
         }
     }
 }
+
+/**
+ * 
+ * @param {number []} arr 要排序的数组
+ * @param {number []} gaps 增量序列
+ */
+function shellSort1(arr, gaps) {
+    for (let g = 0; g < gaps.length; g++) {
+        for (let i = 0; i < arr.length; i++) {
+            let temp = arr[i];
+            let j;
+            for (j = i; j >= gaps[g] &&
+                    arr[j - gaps[g]] > temp; j -= gaps[g]) {
+                // 交换法
+                swap(arr, j, j - gaps[g]);
+            }
+        }
+    }
+
+    function swap(arr, i, j) {  
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
 let arr = [3, 5, 1, 6, 4, 7, 2];
-shellSort(arr, [5, 3, 1]);
+shellSort1(arr, [5, 3, 1]);
 
 console.log(arr);
